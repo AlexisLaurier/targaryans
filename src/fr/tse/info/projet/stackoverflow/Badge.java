c8e325a88764e883d702e947db35ad7256c16253
Ayoub <ayoub@192.168.1.68>
2016-10-18 16:58:22 +0200

    User and Badge creation

---
+++ src/fr/tse/info/projet/stackoverflow/Badge.java
@@ -0,0 +1,5 @@
+package fr.tse.info.projet.stackoverflow;
+
+public class Badge {
+	// Type for extracted badges ( See get_data from json file)
+}
--- src/fr/tse/info/projet/stackoverflow/User.java
+++ src/fr/tse/info/projet/stackoverflow/User.java
@@ -1,5 +1,48 @@
 package fr.tse.info.projet.stackoverflow;
 
 public class User {
+	// User class : defines profile
+	String name; // User's name
+	String password; // Simple String password to acces profile
+	Badge[] badges; // TODO compare to datas extracted from Json files
+
+	public void show() {
+		// Function to show user's details
+		System.out.println("User's name :" + name + "\n" + "User's badges : " // TODO
+																				// add
+																				// toString(badges)
+		);
+
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public User(String name, String password) {
+		super();
+		this.name = name;
+		this.password = password;
+	}
+
+	public boolean comparePassword(String pass) {
+		// Function to compare an entered password to user's
+		if (pass == password) {
+			return true;
+		} else
+			return false;
+	}
 
 }

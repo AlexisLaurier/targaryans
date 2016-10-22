67da51545e011875e950f7343c851559a23123f9
romain <julian-romain@hotmail.fr>
2016-10-22 18:38:26 +0200

    Extraction du Json + Passage du projet en maven

--- src/fr/tse/info/projet/stackoverflow/tools/RequeteGet.java
+++ src/fr/tse/info/projet/stackoverflow/tools/RequeteGet.java
@@ -3,7 +3,15 @@ package fr.tse.info.projet.stackoverflow.tools;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.Reader;
 import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.zip.GZIPInputStream;
+
+import com.google.gson.Gson;
+import com.google.gson.stream.JsonReader;
+
 import java.net.HttpURLConnection;
 
 /**
@@ -11,30 +19,46 @@ import java.net.HttpURLConnection;
  *
  */
 public class RequeteGet {
-	
-	
+
 	/*
 	 * Main function of data recovery from StackExchange API by a GET Method
 	 * 
 	 * @exception IOException
+	 * 
 	 * @param url url to join to have data
+	 * 
 	 * @return the string in JSON return by the API
 	 */
-	private static String recuperationDonnee(String url) throws IOException {
+	private static String recupData(String url) throws IOException {
 
-		String source = "";
-		URL oracle = new URL(url);
-		HttpURLConnection yc = (HttpURLConnection) oracle.openConnection();
-		BufferedReader in = new BufferedReader(new InputStreamReader(yc.getInputStream()));
-		String inputLine;
+		String resultat = "";
+		URL ourUrl = new URL(url);
+		HttpURLConnection connection = (HttpURLConnection) ourUrl.openConnection();
+		connection.setRequestMethod("GET");
+		BufferedReader in = new BufferedReader(new InputStreamReader(new GZIPInputStream(connection.getInputStream())));
 
-		while ((inputLine = in.readLine()) != null)
-			source += inputLine;
-		in.close();
-		return source;
+
+		/*while ((inputLine = in.readLine()) != null)
+			resultat += inputLine;
+		in.close();*/
+		return in.readLine();
+		
+	}
+
+	public static String findContributor(String subject) throws IOException {
+		String contributor = "";
+		Map<Integer, Integer> contributorsMap = new HashMap<Integer, Integer>();
+		contributor = recupData(
+				"https://api.stackexchange.com/2.2/questions/40190834/answers?order=desc&sort=activity&site=stackoverflow");
+		return contributor;
 	}
 
 	public static void main(String[] args) {
-		
+		try {
+			System.out.println(findContributor("coucou"));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
